93394609b9a215395c3e7d88ee97a590d8cfe7e6
Daniel Cefram Ramirez <dcefram@outlook.com>
2020-02-19 09:50:28 +0800

    fix: usage of attach video item in source plugin

--- src/core/item/item.ts
+++ src/core/item/item.ts
@@ -9,7 +9,6 @@ import { ItemConfig, PropertyType } from './types';
 class Item {
   private _internal: Internal;
   private _attributes: any;
-  private _attachProp: string;
   private _isCurrentItem: boolean;
 
   static fromXMLString(xmlString: string, xjs: Xjs) {
@@ -37,14 +36,13 @@ class Item {
     this._internal = config.internal;
     this._attributes = config.attributes;
     this._isCurrentItem = !!config.isCurrentItem;
-    this._attachProp = Environment.isSourcePlugin
-      ? 'AttachVideoItem'
-      : 'SearchVideoItem';
   }
 
   setProperty(prop: PropertyType, param: any): Promise<any> {
     if (prop.setValidator(param)) {
-      this._internal.exec(this._attachProp, this._attributes.id);
+      if (!Environment.isSourcePlugin) {
+        this._internal.exec('SearchVideoItem', this._attributes.id);
+      }
       return this._internal.exec(
         'SetLocalPropertyAsync',
         prop.key,
@@ -57,7 +55,9 @@ class Item {
 
   async getProperty(prop: PropertyType, param: any): Promise<any> {
     if (prop.getValidator(param)) {
-      this._internal.exec(this._attachProp, this._attributes.id);
+      if (!Environment.isSourcePlugin) {
+        this._internal.exec('SearchVideoItem', this._attributes.id);
+      }
       const ret = await this._internal.exec('GetLocalPropertyAsync', prop.key);
       return prop.getTransformer(ret);
     }
