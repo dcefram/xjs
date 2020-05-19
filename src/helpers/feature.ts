488b36cbea11ceaca83b50355b4ee6ad14845021
mato <mato@splitmedialabs.com>
2020-05-19 19:00:55 +0800

    variable fix

--- src/helpers/feature.ts
+++ src/helpers/feature.ts
@@ -1,4 +1,4 @@
 import Internal from '../internal';
 
 export const isSplitMode = async (internal: Internal): Promise<Boolean> =>
-  Boolean(Number(await this.internal.exec('GetGlobalProperty', 'splitmode')));
+  Boolean(Number(await internal.exec('GetGlobalProperty', 'splitmode')));
