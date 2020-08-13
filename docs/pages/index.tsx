005a805208a396d92b3a10b41589d97627b73bad
Daniel Cefram Ramirez <dcefram@outlook.com>
2020-08-13 22:07:38 +0800

    feat: move common layouts to layout component

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,25 +1,11 @@
-import Head from 'next/head';
-import Nav from 'components/Nav';
+import Layout from 'components/layout';
 
 import styles from 'styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>XJS 3.0 (Pre-alpha)</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Nav
-        links={[
-          { label: 'Home', href: '/' },
-          { label: 'Quick Start', href: '/quick-start' },
-          { label: 'Docs', href: '/docs' },
-        ]}
-      />
-
-      <main className={styles.main}>
+    <Layout landing>
+      <div className={styles.main}>
         <div className={styles.overlay}>
           <div className={styles.logo} />
           <div className={styles.subtitle}>
@@ -30,25 +16,7 @@ export default function Home() {
             <button className={styles.getStarted}>GET STARTED WITH 3.0</button>
           </div>
         </div>
-      </main>
-
-      <footer className={styles.copyright}>
-        © 2020{' '}
-        <a
-          href="https://www.splitmedialabs.com/"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          SplitmediaLabs
-        </a>
-        , Ltd. All Rights Reserved.
-      </footer>
-
-      <style jsx>{`
-        footer {
-          margin-bottom: 10px;
-        }
-      `}</style>
-    </div>
+      </div>
+    </Layout>
   );
 }
