377f4abbee4bd2971dcfdac8c326f11415f5d3c5
Daniel Cefram Ramirez <dcefram@outlook.com>
2020-08-13 20:45:01 +0800

    feat: landing page

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Nav from 'components/Nav';
+
+import styles from 'styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -9,6 +11,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Nav
+        links={[
+          { label: 'Home', href: '/' },
+          { label: 'Quick Start', href: '/quick-start' },
+          { label: 'Docs', href: '/docs' },
+        ]}
+      />
+
       <main className={styles.main}>
         <div className={styles.overlay}>
           <div className={styles.logo} />
@@ -17,12 +27,28 @@ export default function Home() {
             Plugins
           </div>
           <div className={styles.buttons}>
-            <button className={styles.getStarted}>GET STARTED</button>
+            <button className={styles.getStarted}>GET STARTED WITH 3.0</button>
           </div>
         </div>
       </main>
 
-      <footer className={styles.copyright}>© 2020 <a href="https://www.splitmedialabs.com/" target="_blank" rel="noreferrer noopener">SplitmediaLabs</a>, Ltd. All Rights Reserved.</footer>
+      <footer className={styles.copyright}>
+        © 2020{' '}
+        <a
+          href="https://www.splitmedialabs.com/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          SplitmediaLabs
+        </a>
+        , Ltd. All Rights Reserved.
+      </footer>
+
+      <style jsx>{`
+        footer {
+          margin-bottom: 10px;
+        }
+      `}</style>
     </div>
   );
 }
