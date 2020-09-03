65eaf06ee084f43d3d95d456686b46fb2fb0270d
Daniel Cefram Ramirez <dcefram@outlook.com>
2020-09-03 19:27:12 +0800

    wip: prevent xjs from throwing errors when executed in a browser

--- src/internal/types.ts
+++ src/internal/types.ts
@@ -1,5 +1,18 @@
 export type CallbackType = (...args: unknown[]) => void;
+
 export type ExecArgument = string | number;
+
 export interface IKeyValuePair {
   [asyncId: string]: CallbackType;
+}
+
+export type ExternalAsyncResponse = string | number | null | undefined;
+
+export interface IXSplitExternal {
+  isXSplitApp(): boolean;
+  CallHost(fn: string, ...args: string[]): ExternalAsyncResponse;
+  AppCallFuncAsync(fn: string, ...args: string[]): ExternalAsyncResponse;
+  AppGetPropertyAsync(prop: string): ExternalAsyncResponse;
+  AppSetPropertyAsync(prop: string, value: string): ExternalAsyncResponse;
+  // @TODO: Complete interface structure based on https://docs.google.com/document/d/1eMBesMZB3WNs6slzG-6Mi9fWKf64cCZiE8oFgf9eTYM/edit#heading=h.14jjkkz4fk1z
 }
