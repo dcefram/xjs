e8f45946e185e06f4ddaac68a562bf2c0af36580
Daniel Cefram Ramirez <dcefram@gmail.com>
2021-02-10 22:16:25 +0800

    wip: squash me

---
+++ src/core/events/events.spec.ts
@@ -0,0 +1,13 @@
+describe('Events', () => {
+  describe('on', function () {
+    it('should add a listener', function () {
+      // @TODO
+    });
+  });
+
+  describe('off', function () {
+    it('should remove a listener', function () {
+      // @TODO
+    });
+  });
+});
