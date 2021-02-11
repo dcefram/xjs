804afc528b90027d43ff9ca2fbaf4f9380c053b4
Daniel Cefram Ramirez <dcefram@gmail.com>
2021-02-11 22:46:03 +0800

    wip

--- src/core/events/events.spec.ts
+++ src/core/events/events.spec.ts
@@ -1,13 +1,29 @@
+import Events from './events';
+import Xjs from 'core/xjs';
+jest.mock('../xjs');
+
 describe('Events', () => {
+  let events;
+
+  beforeAll(() => {
+    const xjs = new Xjs();
+    events = new Events(xjs);
+  });
+
   describe('on', function () {
-    it('should add a listener', function () {
-      // @TODO
+    it('should add a listener', function (done) {
+      events.on('SceneChange', (arg) => {
+        expect(arg).toBe('testing');
+        done();
+      });
+
+      window.AppOnEvent('testing');
     });
   });
 
-  describe('off', function () {
-    it('should remove a listener', function () {
-      // @TODO
-    });
-  });
+  // describe('off', function () {
+  //   it('should remove a listener', function () {
+  //     // @TODO
+  //   });
+  // });
 });
--- types/window.d.ts
+++ types/window.d.ts
@@ -5,6 +5,7 @@ declare global {
     OnAsyncCallback: any;
     OnPropsMessageReceive: (payload: string) => any;
     SetEvent: (value: string) => void;
+    AppOnEvent: (value: string) => void;
     OnDialogResult: Function;
   }
 }
