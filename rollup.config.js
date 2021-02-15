6095e3477bc13583325aa90f977570e906d1c99a
Daniel Cefram Ramirez <dcefram@gmail.com>
2021-02-15 21:49:21 +0800

    chore: update version and dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,11 @@
 import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import builtins from 'rollup-plugin-node-builtins';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'tools/umd.entry.ts',
   plugins: [
-    builtins(),
     typescript(),
     resolve({
       browser: true,
