498021a47570d8d168449e1c08b6a40f45f050ca
Daniel Cefram S. Ramirez <dc@rmrz.ph>
2021-03-26 14:05:25 +0800

    feat: add entry file

---
+++ src/core/extension-window/index.ts
@@ -0,0 +1,26 @@
+import getMode from './get-mode';
+import setSize, {
+  setMinimumWindowSize as setMinimumSize,
+  setMaximumWindowSize as setMaximumSize,
+} from './set-size';
+import setStyle from './set-style';
+import setTitle from './set-title';
+import close from './close';
+import minimize from './minimize';
+import maximize from './maximize';
+import move from './move';
+
+const extensionWindow = {
+  getMode,
+  setSize,
+  setMinimumSize,
+  setMaximumSize,
+  setStyle,
+  setTitle,
+  close,
+  minimize,
+  maximize,
+  move,
+};
+
+export default extensionWindow;
